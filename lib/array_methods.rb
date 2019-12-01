14bd28b4c71c1d9f0ec1eb66cef032a582da44a7
Asif Kamal <asif_kamal@hotmail.com>
2019-12-01 01:48:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,7 @@ def find_element_index(array, value_to_find)
   length = array.length
   length.times do |index|
   end
-  if |index| = value_to_find
-  else nil
+  |index| = value_to_find
 end
 
 def find_max_value(array)
